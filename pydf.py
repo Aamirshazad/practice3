f78e3a7ac74640818f6f692ad41d4f89ac428d57
Aamirshazad <amiralicomsats3@gmail.com>
2024-01-21 19:40:59 +0500

    Add arithmetic function in code

--- pydf.py
+++ pydf.py
@@ -1,1 +1,3 @@
-print("hay")
+print("Add new code ")
+print("Add new code 2 ")
+print("Add new code 3 ")
--- python.py
+++ python.py
@@ -2,6 +2,17 @@ print("Hello world")
 print("Hello world")
 print("Hello world")
 print("Hello world")
-print("lets chnge")
 
-print("lets chnge")
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+print(add(1,2))
+print(sub(1,2))
+print(mul(1,2))
+
