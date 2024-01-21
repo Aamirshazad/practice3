006e97dc50d9bf3e1668a3dc96781711cd2daf77
Aamirshazad <amiralicomsats3@gmail.com>
2024-01-21 19:57:01 +0500

    add even odd function

--- amir.py
+++ amir.py
@@ -1,2 +1,9 @@
 print("This is python file we just us e it to practice git")
-print("This is python file we just us e it to practice git")
+
+def even(a):
+    if a // 2 == 0:
+        return True
+    else:
+        return False
+    
+
