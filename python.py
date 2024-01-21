36535d55e8f160921f87e4dfc3561b913de53ab1
Aamirshazad <amiralicomsats3@gmail.com>
2024-01-21 20:21:51 +0500

    add code again

--- python.py
+++ python.py
@@ -5,17 +5,21 @@ print("Hello world")
 def add(a,b):
     return a+b
  
- def sub(a,b):
+def sub(a,b):
     return a-b
+
+def div(a,b):
+    return a/b
+
 
 
 
 def mul(a,b):
     return a*b
-
+print(div(1,2))
 print(add(1,2))
 print(sub(1,2))
 print(mul(1,2))
-print(div(1,2))
+
 
 
